e51acf36cec6836e88f8c292fe761041dccca6d0
Poteryashka163 <scrat0072@gmail.com>
2023-03-16 22:42:46 +0400

    шаблон

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,23 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        }
     }
     public static void task2() {
         System.out.println("Задача 2");
+        int clientOS = 1;
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+        if (clientOS == 0){
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        }
     }
     public static void task3() {
         System.out.println("Задача 3");
